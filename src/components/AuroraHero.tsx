a46ca11b1965a1d54820458ae638317fde2f6ce0
Projects <projects.done.100@gmail.com>
2025-08-19 12:10:46 +0000

    great color part is done ./ but the stars behind component is still rema

--- src/components/AuroraHero.tsx
+++ src/components/AuroraHero.tsx
@@ -1,29 +1,25 @@
 'use client';
 
-import React, { useEffect } from "react";
-import {
-  useMotionTemplate,
-  useMotionValue,
-  motion,
-  animate,
-} from "framer-motion";
+import React, { useEffect } from 'react';
+import { useMotionTemplate, useMotionValue, motion, animate } from 'framer-motion';
+import Starfield from '@/components/Starfield';
 
-const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
+const COLORS_TOP = ['#13FFAA', '#1E67C6', '#CE84CF', '#DD335C'];
 
 export const AuroraHero = ({ children }: { children: React.ReactNode }) => {
   const color = useMotionValue(COLORS_TOP[0]);
 
   useEffect(() => {
     animate(color, COLORS_TOP, {
-      ease: "easeInOut",
+      ease: 'easeInOut',
       duration: 10,
       repeat: Infinity,
-      repeatType: "mirror",
+      repeatType: 'mirror',
     });
   }, []);
 
   const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
-  
+
   return (
     <motion.section
       style={{
@@ -31,9 +27,8 @@ export const AuroraHero = ({ children }: { children: React.ReactNode }) => {
       }}
       className="relative overflow-hidden bg-gray-950 text-gray-200"
     >
-      <div className="relative z-10">
-          {children}
-      </div>
+      <Starfield />
+      <div className="relative z-10">{children}</div>
     </motion.section>
   );
 };
